f56b8f4bb2b1561f8fd5abb96734dcb07022dc01
zibredlektab <zibredlektab@gmail.com>
2024-06-24 13:29:03 -0400

    Rearranged some stuff to prioritize button monitoring & put power monitoring last...untested

--- QTPycode.py
+++ QTPycode.py
@@ -4,6 +4,7 @@ import digitalio
 import busio
 import usb_hid
 import neopixel
+import supervisor
 
 from adafruit_tla202x import TLA2024
 from adafruit_tla202x.analog_in import AnalogIn
@@ -48,7 +49,7 @@ hold_delay = 7.0
 # i2c stuff
 
 try:
-    
+
     pixel.fill((10,255,255)) #cyan
     i2cbus = busio.I2C(board.SCL, board.SDA)
     tla = TLA2024(i2cbus)
@@ -56,25 +57,99 @@ except:
     print("no i2c")
     pixel.fill((255,0,0)) #red
     while True:
-        time.sleep(5)
-    
+        time.sleep(2)
+    #power_on.value = False
+
 vdc = 0.0
 vbatt = 0.0
-
-
-pixel.fill((255,0, 200)) #pink
+temp_vdc = 0.0
+temp_vbatt = 0.0
+total_samples = 5
+sample_count = 0
 
 while True:
-   
+
+    if time.monotonic() > time_to_start_keyboard:
+        if not keyboard_started:
+            # Start up keyboard
+            pixel.fill((150,150,0)) #yellow
+
+            if supervisor.runtime.usb_connected:
+                try:
+                    keyboard = Keyboard(usb_hid.devices)
+                    Keyboard_layout = KeyboardLayoutUS(keyboard)
+                    keyboard_started = True
+                except:
+                    keyboard_started = False
+            else:
+                keyboard_started = False
+
+
+    # Button monitoring
+    if not button.value: # button is grounded, ie pushed
+        if button_is_pushed: # button was previously being pushed, still being pushed
+            if time_push_began + hold_delay < time.monotonic(): # hold delay has been surpassed
+                if not button_is_held:
+                    print("...and held...")
+                    button_is_held = True
+                    if keyboard_started == True:
+                        keyboard.press(Keycode.CONTROL)
+                        keyboard.press(Keycode.ALT)
+                        keyboard.press(Keycode.F1)
+                        keyboard.release_all()
+                    # do long-press things
+        else : # button was not previously being pushed
+            if time_released + debounce < time.monotonic(): # only register new press once debounce has been surpassed
+                print("button was pushed...")
+                button_is_pushed = True
+                time_push_began = time.monotonic() # store current time
+
+    else: # button is not currently being pushed
+        if button_is_pushed: # button was previously being pushed, so it has been released
+            if time_push_began + hold_delay < time.monotonic(): # hold delay has not been surpassed
+                print("...and released (long press)")
+                pixel.fill((0,0,0))
+                time.sleep(.1)
+                pixel.fill((0,0,255)) # blue
+                time.sleep(.1)
+                pixel.fill((0,0,0))
+                time.sleep(.1)
+                pixel.fill((0,0,255)) # blue
+                time.sleep(.1)
+                power_on.value = False # turn off power
+            else:
+                print("...and released (short press)")
+                # do shot-press things
+                if keyboard_started == True:
+                    keyboard.press(Keycode.CONTROL)
+                    keyboard.press(Keycode.ALT)
+                    keyboard.press(Keycode.ESCAPE)
+                    keyboard.release_all()
+                pixel.fill((0,0,0))
+                time.sleep(.1)
+                pixel.fill((0,255,0)) # green
+                time.sleep(.1)
+            button_is_pushed = False
+            button_is_held = False
+            time_released = time.monotonic()
+            pixel.fill((255,0,0))
+            
+            
     # Power monitoring
-    tla.input_channel = 2
-    vdc = tla.voltage
-    tla.input_channel = 0
-    vbatt = tla.voltage
-    print("VDC: ", vdc, "V, VBatt: ", vbatt, "V")
-    
+    if sample_count < total_samples: # Smooth out samples
+        tla.input_channel = 2
+        temp_vdc += tla.voltage
+        tla.input_channel = 0
+        temp_vbatt += tla.voltage
+        sample_count += 1
+    else:
+        vdc = temp_vdc / sample_count
+        vbatt = temp_vdc / sample_count
+        sample_count = 0
+        
+
     if vdc > 1.0: # Running on DC power, not battery
-        pixel.fill((0, 255, 0))
+        pixel.fill((0, 255, 0)) # green
     else: # Running on battery
         if vbatt > 4:
             pixel.fill((0, 0, 255)) # blue
@@ -82,66 +157,5 @@ while True:
             pixel.fill((255, 128, 0)) # orange
         else:
             pixel.fill((255, 0, 0)) # red
-
-    if time.monotonic() > time_to_start_keyboard:
-        if not keyboard_started:
-            # Start up keyboard
-            pixel.fill((150,150,0)) #yellow
-            
-            try:
-                keyboard = Keyboard(usb_hid.devices)
-                Keyboard_layout = KeyboardLayoutUS(keyboard)
-                keyboard_started = True
-            except:
-                pixel.fill((100,0,0))
-                while True:
-                    time.sleep(5)
-
-        # Button monitoring
-        if not button.value: # button is grounded, ie pushed
-            if button_is_pushed: # button was previously being pushed, still being pushed
-                if time_push_began + hold_delay < time.monotonic(): # hold delay has been surpassed
-                    if not button_is_held:
-                        print("...and held...")
-                        button_is_held = True
-                        keyboard.press(Keycode.CONTROL)
-                        keyboard.press(Keycode.ALT)
-                        keyboard.press(Keycode.F1)
-                        keyboard.release_all()
-                        # do long-press things
-            else : # button was not previously being pushed
-                if time_released + debounce < time.monotonic(): # only register new press once debounce has been surpassed
-                    print("button was pushed...")
-                    button_is_pushed = True
-                    time_push_began = time.monotonic() # store current time
-
-        else: # button is not currently being pushed
-            if button_is_pushed: # button was previously being pushed, so it has been released
-                if time_push_began + hold_delay < time.monotonic(): # hold delay has not been surpassed
-                    print("...and released (long press)")
-                    pixel.fill((0,0,0))
-                    time.sleep(.1)
-                    pixel.fill((0,0,255))
-                    time.sleep(.1)
-                    pixel.fill((0,0,0))
-                    time.sleep(.1)
-                    pixel.fill((0,0,255))
-                    time.sleep(.1)
-                    power_on.value = False # turn off power
-                else:
-                    print("...and released (short press)")
-                    # do shot-press things
-                    keyboard.press(Keycode.CONTROL)
-                    keyboard.press(Keycode.ALT)
-                    keyboard.press(Keycode.ESCAPE)
-                    keyboard.release_all()
-                    pixel.fill((0,0,0))
-                    time.sleep(.1)
-                    pixel.fill((0,255,0))
-                    time.sleep(.1)
-                button_is_pushed = False
-                button_is_held = False
-                time_released = time.monotonic()
-                pixel.fill((255,0,0))
 
     time.sleep(0.01)
